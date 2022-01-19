b8a2882f51aeccca927715667fdf6611f1e3645a
Taichi Soma <sosomasox@gmail.com>
2022-01-19 23:40:37 +0900

    Update

--- undeploy4node.sh
+++ undeploy4node.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 
-seq 1 3 | xargs -I {} -P 3 scp uninstall4node.sh control-plane-{}.k8s.home.arpa:
-seq 1 3 | xargs -I {} -P 3 ssh control-plane-{}.k8s.home.arpa "./uninstall4node.sh"
+if [ "$1" = "control-plane" ]; then
+    seq 1 3 | xargs -I {} -P 3 scp uninstall4node.sh control-plane-{}.k8s.home.arpa:
+    seq 1 3 | xargs -I {} -P 3 ssh control-plane-{}.k8s.home.arpa "./uninstall4node.sh"
+fi
 
 seq 1 5 | xargs -I {} -P 5 scp uninstall4node.sh node-{}.k8s.home.arpa:
 seq 1 5 | xargs -I {} -P 5 ssh node-{}.k8s.home.arpa "./uninstall4node.sh"
