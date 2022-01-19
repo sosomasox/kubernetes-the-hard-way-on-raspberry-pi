b97b0df13f436261fa6aed7c5aa2f7b42c04f74b
Taichi Soma <sosomasox@gmail.com>
2022-01-19 19:28:53 +0900

    Update

--- deploy4node.sh
+++ deploy4node.sh
@@ -50,8 +50,13 @@ done
 }
 
 
-./scp4node.sh
-DEPLOY_CONTROL-PLANE
-DEPLOY_NODE
+if [ "$1" = "control-plane" ]; then
+    ./scp4node.sh $1
+    DEPLOY_CONTROL-PLANE
+    DEPLOY_NODE
+else
+    ./scp4node.sh
+    DEPLOY_NODE
+fi
 
 exit 0
