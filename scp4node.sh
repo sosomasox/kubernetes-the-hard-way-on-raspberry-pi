5306c248f6a64659b907e49061113aa81ea0e889
Taichi Soma <sosomasox@gmail.com>
2022-01-19 19:28:06 +0900

    Update

--- scp4node.sh
+++ scp4node.sh
@@ -1,39 +1,53 @@
-#!/bin/bash
+#!/bin/bash -x
 
+SCP_CONTROL-PLANE ()
+{
 for i in `seq 1 3`
 do
-    scp \
-        certs/cert/kubernetes-ca.pem \
-        certs/cert/kubernetes-front-proxy-ca.pem \
-        certs/cert/control-plane-${i}.pem \
-        certs/cert/control-plane-${i}-key.pem \
-        kubeconfig/control-plane-${i}.kubeconfig \
-        kubeconfig/kube-proxy.kubeconfig \
-        containerd/config/config.toml \
-        containerd/unitfile/containerd.service \
-        configs/kube-proxy/kube-proxy-config.yaml \
-        configs/kubelet/control-plane-${i}/kubelet-config.yaml \
-        unitfiles/kube-proxy/kube-proxy.service \
-        unitfiles/kubelet/control-plane-${i}/kubelet.service \
-        control-plane-${i}.k8s.home.arpa:
+scp \
+    certs/cert/kubernetes-ca.pem \
+    certs/cert/kubernetes-front-proxy-ca.pem \
+    certs/cert/control-plane-${i}.pem \
+    certs/cert/control-plane-${i}-key.pem \
+    kubeconfig/control-plane-${i}.kubeconfig \
+    kubeconfig/kube-proxy.kubeconfig \
+    containerd/config/config.toml \
+    containerd/unitfile/containerd.service \
+    configs/kube-proxy/kube-proxy-config.yaml \
+    configs/kubelet/control-plane-${i}/kubelet-config.yaml \
+    unitfiles/kube-proxy/kube-proxy.service \
+    unitfiles/kubelet/control-plane-${i}/kubelet.service \
+    control-plane-${i}.k8s.home.arpa:
 done
+}
 
+
+SCP_NODE ()
+{
 for i in `seq 1 5`
 do
-    scp \
-        certs/cert/kubernetes-ca.pem \
-        certs/cert/kubernetes-front-proxy-ca.pem \
-        certs/cert/node-${i}.pem \
-        certs/cert/node-${i}-key.pem \
-        kubeconfig/node-${i}.kubeconfig \
-        kubeconfig/kube-proxy.kubeconfig \
-        containerd/config/config.toml \
-        containerd/unitfile/containerd.service \
-        configs/kube-proxy/kube-proxy-config.yaml \
-        configs/kubelet/node-${i}/kubelet-config.yaml \
-        unitfiles/kube-proxy/kube-proxy.service \
-        unitfiles/kubelet/node-${i}/kubelet.service \
-        node-${i}.k8s.home.arpa:
+scp \
+    certs/cert/kubernetes-ca.pem \
+    certs/cert/kubernetes-front-proxy-ca.pem \
+    certs/cert/node-${i}.pem \
+    certs/cert/node-${i}-key.pem \
+    kubeconfig/node-${i}.kubeconfig \
+    kubeconfig/kube-proxy.kubeconfig \
+    containerd/config/config.toml \
+    containerd/unitfile/containerd.service \
+    configs/kube-proxy/kube-proxy-config.yaml \
+    configs/kubelet/node-${i}/kubelet-config.yaml \
+    unitfiles/kube-proxy/kube-proxy.service \
+    unitfiles/kubelet/node-${i}/kubelet.service \
+    node-${i}.k8s.home.arpa:
 done
+}
+
+
+if [ "$1" = "control-plane" ]; then
+    SCP_CONTROL-PLANE
+fi
+
+SCP_NODE
 
 exit 0
