f5d6a5e418f2c5c3df9cd94f9abd41749d2e3807
Taichi Soma <sosomasox@gmail.com>
2022-01-21 00:45:09 +0900

    Update to select reboot target

--- reboot.sh
+++ reboot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 
-for i in `seq 1 3`; do ssh control-plane-${i}.k8s.home.arpa "sudo reboot"; done
-for i in `seq 1 5`; do ssh node-${i}.k8s.home.arpa "sudo reboot"; done
+if [ "$1" = "control-plane" ]; then
+    for i in `seq 1 3`; do ssh control-plane-${i}.k8s.home.arpa "sudo reboot"; done
+elif [ "$1" = "node" ]; then
+    for i in `seq 1 5`; do ssh node-${i}.k8s.home.arpa "sudo reboot"; done
+fi
 
 exit 0
