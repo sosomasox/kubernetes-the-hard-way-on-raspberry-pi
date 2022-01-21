e3ece77d9eca6b75fab2697a6cc706289dad00c0
Taichi Soma <sosomasox@gmail.com>
2022-01-21 12:28:06 +0900

    Update to able to select args "all"

--- reboot.sh
+++ reboot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 
-if [ "$1" = "control-plane" ]; then
+if [ "$1" = "all" ]; then
+    for i in `seq 1 3`; do ssh control-plane-${i}.k8s.home.arpa "sudo reboot"; done
+    for i in `seq 1 5`; do ssh node-${i}.k8s.home.arpa "sudo reboot"; done
+elif [ "$1" = "control-plane" ]; then
     for i in `seq 1 3`; do ssh control-plane-${i}.k8s.home.arpa "sudo reboot"; done
 elif [ "$1" = "node" ]; then
     for i in `seq 1 5`; do ssh node-${i}.k8s.home.arpa "sudo reboot"; done
