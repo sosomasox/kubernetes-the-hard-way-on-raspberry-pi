6feeecc4e68de8aa3ca10aed5c9bb71d0aa5e6d0
Taichi Soma <sosomasox@gmail.com>
2022-01-26 19:00:54 +0900

    Update

--- deploy4etcd.sh
+++ deploy4etcd.sh
@@ -4,6 +4,8 @@
 
 seq 1 3 | xargs -I {} -P 3 scp install4etcd.sh etcd-{}.k8s.home.arpa:
 seq 1 3 | xargs -I {} -P 3 ssh etcd-{}.k8s.home.arpa "./install4etcd.sh"
+seq 1 3 | xargs -I {} -P 3 ssh etcd-{}.k8s.home.arpa "rm ./install4etcd.sh"
+
 for i in `seq 1 3`
 do 
     ssh etcd-${i}.k8s.home.arpa "sudo mv etcd-*.pem /etc/etcd/pki/"
