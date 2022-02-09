8348524d4acfd93872703c93c0f32d17decce899
Taichi Soma <sosomasox@gmail.com>
2022-02-10 02:17:29 +0900

    Update to add fs.inotify.max_user_watches setting

--- install4node.sh
+++ install4node.sh
@@ -25,6 +25,7 @@ sudo sh -c "cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
+fs.inotify.max_user_watches         = 20480
 EOF"
 
 sudo sysctl --system
