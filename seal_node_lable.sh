b51a641ff4ec06c9e5488575fdbc65104834918f
Taichi Soma <sosomasox@gmail.com>
2022-03-12 14:20:20 +0900

    Update

--- seal_node_lable.sh
+++ seal_node_lable.sh
@@ -8,9 +8,9 @@ kubectl label node control-plane-1.k8s.home.arpa node-role.kubernetes.io/control
 kubectl label node control-plane-2.k8s.home.arpa node-role.kubernetes.io/control-plane=''
 kubectl label node control-plane-3.k8s.home.arpa node-role.kubernetes.io/control-plane=''
 
-kubectl label node control-plane-1.k8s.home.arpa node-role.kubernetes.io/node=''
-kubectl label node control-plane-2.k8s.home.arpa node-role.kubernetes.io/node=''
-kubectl label node control-plane-3.k8s.home.arpa node-role.kubernetes.io/node=''
+kubectl taint nodes control-plane-1.k8s.home.arpa node-role.kubernetes.io/control-plane:NoSchedule
+kubectl taint nodes control-plane-2.k8s.home.arpa node-role.kubernetes.io/control-plane:NoSchedule
+kubectl taint nodes control-plane-3.k8s.home.arpa node-role.kubernetes.io/control-plane:NoSchedule
 
 kubectl label node node-1.k8s.home.arpa node-role.kubernetes.io/node=''
 kubectl label node node-2.k8s.home.arpa node-role.kubernetes.io/node=''
